d1e483413cf39eb5f8edb31c1d2508223e1915a0
SadiaAfrinMim <mimsadia367@gmail.com>
2025-03-20 16:42:05 +0600

    function a type define

--- function/index.ts
+++ function/index.ts
@@ -1,7 +1,18 @@
-function sayHi(personName:string ,age:number){
+// function sayHi(personName:string ,age:number){
   
 
-    console.log("Hi",personName,"your age",age);
+//     console.log("Hi",personName,"your age",age);
    
+// }
+// sayHi("Rafi",10)
+
+// function addition(number1:number,number2:number):string{
+//     return `yout total number is ${number1+number2}`;
+// }
+// addition(10,19)
+
+function multiply(number1:number,number2:number=20):number{
+    return number1 * number2;
 }
-sayHi("Rafi",10)
+
+multiply(10)
