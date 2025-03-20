24133a3c9474cdc7f54f8d88004b4018385cf9c5
SadiaAfrinMim <mimsadia367@gmail.com>
2025-03-21 00:37:01 +0600

    interface done

--- interface/index.ts
+++ interface/index.ts
@@ -11,20 +11,29 @@ type people1 = {
 interface people {
     fristName:string,
     lastName: string,
-    age:number
-}
-interface people{
+    age:number,
     email: string,
     fullNme():string
 }
+interface Family{
+    isWife: boolean,
 
-let sadia:people = {
+}
+interface Employee extends people,Family{
+    designation: String,
+    yearOfExperience: number
+}
+
+let sadia: Employee = {
     fristName:"sadia afrin",
     lastName:" mim",
     age: 88,
     email:"sadiaafrinmim660@gmail.com",
     fullNme: function(){
         return`${this.fristName} ${this.lastName}`
-    }
+    },
+    designation:"fronend developer",
+    yearOfExperience: 10,
+    isWife: true,
 }
 console.log(sadia.fullNme())
