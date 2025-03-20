23f637982fc261e6795e2ed9eeb94d4700d12891
SadiaAfrinMim <mimsadia367@gmail.com>
2025-03-20 23:40:57 +0600

    type narrowing done

---
+++ Narrowing_Type/index.ts
@@ -0,0 +1,10 @@
+function sayDetails(name:string, age:string |number):void{
+    if(typeof age === "string"){
+      currentAge = Number(age) -2 
+        
+    }
+    else{
+        currentAge = age -2
+    }
+    console.log(`y9our name is : ${name}, your age is :${currentAge}`)
+}
