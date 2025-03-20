106e5e579a74ea18381eea52b81560c3cd252505
SadiaAfrinMim <mimsadia367@gmail.com>
2025-03-20 16:00:00 +0600

    type anonntation

--- types/index.ts
+++ types/index.ts
@@ -1,1 +1,10 @@
-let nameOfPlaylist:number =  10;
+// type anontation
+
+// let numOfPlaylist:number =  10;
+// numOfPlaylist +=1;
+// numOfPlaylist -=1;
+
+// numOfPlaylist ="Nine"
+
+let friendName:string = "Alex";
+friendName ="sadia afrin mim"
