cd1309773cff6fd811366077dcff474c229a1888
SadiaAfrinMim <mimsadia367@gmail.com>
2025-03-20 16:09:09 +0600

    type inference

--- types/index.ts
+++ types/index.ts
@@ -6,5 +6,32 @@
 
 // numOfPlaylist ="Nine"
 
-let friendName:string = "Alex";
+// ! aeta type inference --je type diyaci seta e  nibe
+
+// let friendName:string = "Alex";
+// friendName ="sadia afrin mim"
+// friendName = true
+
+// !any dile je kono  type nibe
+let friendName:any = "Alex";
 friendName ="sadia afrin mim"
+friendName = true
+
+
+// ?any r moto kaj korbe sudu matro assign kore dile
+
+// let intro;
+//  intro = "sadia afrin mim";
+//  into =10;
+
+// *boolean type 
+// let isPassed:boolean = true;
+// isPassed = false;
+// isPassed =10
+
+
+// ? complex ==== dui ta ak satha dite pari
+let numberOfPlayList:number | string |boolean = 11;
+numberOfPlayList =11;
+numberOfPlayList ="eleven"
+numberOfPlayList = true
