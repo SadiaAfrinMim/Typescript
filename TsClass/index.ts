93441113745f5e7f7c1ac610c84a2e031218236b
SadiaAfrinMim <mimsadia367@gmail.com>
2025-03-21 15:13:20 +0600

    calss part complete

--- TsClass/index.ts
+++ TsClass/index.ts
@@ -1,10 +1,36 @@
-class Computers{
-   private _ssd:number
-    constructor(public name:string, public ram:number){
-        this.name = name;
-        this.ram= ram;
+// class Computers{
+//    private _ssd:number
+//     constructor(public name:string, public ram:number ){
+//         this.name = name;
+//         this.ram= ram;
 
-        }
+//         }
+//         private getDetails():void{
+//             console.log(`computer name: ${this.name}`)
+//         }
+//         get ssdDetails():number{
+//             return this._ssd;
+//         }
+//     }
+
+//     const computer1= new Computers("hp",16)
+
+
+// interface 
+
+interface ComputerInterface{
+    name:string,
+    ram: number,
+}
+interface DetailsInterface{
+    aboutComputers():void
+}
+
+class Computers implements ComputerInterface,DetailsInterface{
+    constructor(public name:string,public ram:number){}
+    aboutComputers(): void{
+        console.log(`computer name ${this.name} ,and ram:${this.ram}`)
     }
+}
 
-    const computer1= new Computers("hp",16)
+let computer = new Computers("hp",18)
